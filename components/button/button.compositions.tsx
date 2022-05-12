210b7c1d28d93877ee87f9c6876c247aa1c000f5
Leonardo Coutinho dos Santos <leonardo.coutinho@vetta.digital>
2022-05-12 01:49:58 -0300

    editing README.md file

--- components/button/button.compositions.tsx
+++ components/button/button.compositions.tsx
@@ -1,22 +1,24 @@
 import React from 'react';
 import { Button } from './button';
 
+export interface ButtonProps {}
+
 export const PrimaryButton = () => {
-  return <Button text='Clique aqui'/>
+  return <Button text='Primary Button'/>
 }
 
 export const SecondaryButton = () => {
-  return <Button text='Clique aqui'/>
+  return <Button text='Secondary Button'/>
 }
 
 export const DangerButton = () => {
-  return <Button text='Clique aqui'/>
+  return <Button text='Danger Button'/>
 }
 
 export const WarningButton = () => {
-  return <Button text='Clique aqui'/>
+  return <Button text='Warning Button'/>
 }
 
 export const DeleteButton = () => {
-  return <Button text='Clique aqui'/>
+  return <Button text='Delete Button'/>
 }
