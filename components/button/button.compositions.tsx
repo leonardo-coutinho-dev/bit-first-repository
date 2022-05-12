b05981d62c3a26e8a193cb54d37cdeaf3f568bbf
Leonardo Coutinho dos Santos <leonardo.coutinho@vetta.digital>
2022-05-12 01:37:29 -0300

    creating readme file

--- components/button/button.compositions.tsx
+++ components/button/button.compositions.tsx
@@ -1,6 +1,22 @@
 import React from 'react';
 import { Button } from './button';
 
-export const primaryButton = () => {
+export const PrimaryButton = () => {
+  return <Button text='Clique aqui'/>
+}
+
+export const SecondaryButton = () => {
+  return <Button text='Clique aqui'/>
+}
+
+export const DangerButton = () => {
+  return <Button text='Clique aqui'/>
+}
+
+export const WarningButton = () => {
+  return <Button text='Clique aqui'/>
+}
+
+export const DeleteButton = () => {
   return <Button text='Clique aqui'/>
 }
